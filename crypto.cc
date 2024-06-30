fffae2abaaccfcb342a333fe94ea2ef2506aa436
callsamu <samuelbree@gmail.com>
2024-06-30 02:25:24 -0300

    feat: rodar clang-format com estilo da Google

--- crypto.cc
+++ crypto.cc
@@ -3,62 +3,55 @@
 #include <string_view>
 #include <vector>
 
-const std::byte INVERT_MASK      {0x03};
-const std::byte FIRST_HALF_MASK  {0xF0};
-const std::byte SECOND_HALF_MASK {0x0F};
+const std::byte INVERT_MASK{0x03};
+const std::byte FIRST_HALF_MASK{0xF0};
+const std::byte SECOND_HALF_MASK{0x0F};
 
-const std::string_view MESSAGE {R"(
+const std::string_view MESSAGE{R"(
 	10010110 11110111 01010110 00000001 
 	00010111 00100110 01010111 00000001 
 	00010111 01110110 01010111 00110110 
 	11110111 11010111 01010111 00000011
 )"};
 
-constexpr bool 
-is_binary(char const c) {
-	return c == '0' || c == '1';
+constexpr bool is_binary(char const c) { return c == '0' || c == '1'; }
+
+std::vector<std::byte> parse(std::string_view message) {
+  std::vector<std::byte> bytes{};
+
+  auto is_in_number = false;
+  std::byte b{0};
+
+  for (auto const c : message) {
+    if (is_binary(c)) {
+      is_in_number = true;
+      auto const digit = std::byte(c == '0' ? 0 : 1);
+      b = (b << 1) | digit;
+    } else {
+      if (is_in_number) {
+        is_in_number = false;
+        bytes.push_back(b);
+        b = std::byte(0);
+      }
+    }
+  }
+
+  return bytes;
 }
 
-std::vector<std::byte> 
-parse(std::string_view message) {
-	std::vector<std::byte> bytes{};
+char decrypt(std::byte b) {
+  b ^= INVERT_MASK;
 
-	auto is_in_number = false;
-	std::byte b{0};
+  b = ((b & FIRST_HALF_MASK) >> 4) | ((b & SECOND_HALF_MASK) << 4);
 
-	for (auto const c : message) {
-		if (is_binary(c)) {
-			is_in_number = true;
-			auto const digit = std::byte(c == '0' ? 0 : 1);
-			b = (b << 1) | digit;
-		} else {
-			if (is_in_number) {
-				is_in_number = false;
-				bytes.push_back(b);
-				b = std::byte(0);
-			}
-		}
-	}
-
-	return bytes;
+  return std::to_integer<char>(b);
 }
 
-char 
-decrypt(std::byte b) {
-	b ^= INVERT_MASK;
+int main() {
+  auto const bytes = parse(MESSAGE);
 
-	b = ((b & FIRST_HALF_MASK) >> 4) |
-		((b & SECOND_HALF_MASK) << 4);
-
-	return std::to_integer<char>(b);
-}
-
-int 
-main() {
-	auto const bytes = parse(MESSAGE);
-
-	for (auto const b : bytes) {
-		std::cout << decrypt(b);
-	}
-	std::cout << '\n';
+  for (auto const b : bytes) {
+    std::cout << decrypt(b);
+  }
+  std::cout << '\n';
 }
--- distance.cc
+++ distance.cc
@@ -6,93 +6,74 @@
 #include <utility>
 #include <vector>
 
-uint64_t
-distance(int64_t const a, int64_t const b) {
-	return std::abs(a - b);
+uint64_t distance(int64_t const a, int64_t const b) { return std::abs(a - b); }
+
+void insertion_sort(std::vector<int64_t>& v) {
+  auto const size = v.size();
+  if (size == 1) return;
+
+  for (size_t i = 1; i < size; i++) {
+    auto const key = v.at(i);
+    int64_t j = i - 1;
+
+    while ((j >= 0) && (v.at(j) > key)) {
+      v.at(j + 1) = v.at(j);
+      --j;
+    }
+
+    v.at(j + 1) = key;
+  }
 }
 
-void 
-insertion_sort(std::vector<int64_t>& v) {
-	auto const size = v.size();
-	if (size == 1) return;
+uint64_t shortest_distance(std::vector<int64_t> const& A,
+                           std::vector<int64_t> const& B) {
+  size_t cursor{0};
+  size_t on_hold_cursor{0};
 
-	for (size_t i = 1; i < size; i++) {
-		auto const key = v.at(i);
-		int64_t j = i - 1;
+  auto selected = A[0] < B[0] ? A : B;
+  auto on_hold = A[0] > B[0] ? A : B;
 
-		while ((j >= 0) && (v.at(j) > key)) {
-			v.at(j + 1) = v.at(j);
-			--j;
-		}
+  auto min_diff = distance(A[0], B[0]);
 
-		v.at(j + 1) = key;
-	}
+  while (cursor < selected.size()) {
+    if (selected.at(cursor) >= on_hold.at(on_hold_cursor)) {
+      min_diff = std::min(min_diff, distance(selected.at(cursor - 1),
+                                             on_hold.at(on_hold_cursor)));
+
+      std::swap(selected, on_hold);
+      std::swap(cursor, on_hold_cursor);
+    } else {
+      ++cursor;
+    }
+  }
+
+  min_diff = std::min(
+      min_diff, distance(selected.at(cursor - 1), on_hold.at(on_hold_cursor)));
+
+  return min_diff;
 }
 
-uint64_t
-shortest_distance(
-	std::vector<int64_t> const& A, 
-	std::vector<int64_t> const& B
-) {
-	size_t cursor{0};
-	size_t on_hold_cursor{0};
-
-	auto selected = A[0] < B[0] ? A : B;
-	auto on_hold = A[0] > B[0] ? A : B; 
-
-	auto min_diff = distance(A[0], B[0]);
-
-	while (cursor < selected.size()) {
-		if (selected.at(cursor) >= on_hold.at(on_hold_cursor)) {
-			min_diff = std::min(
-				min_diff, 
-				distance(
-					selected.at(cursor - 1), 
-					on_hold.at(on_hold_cursor)
-				)
-			);
-
-			std::swap(selected, on_hold);
-			std::swap(cursor, on_hold_cursor);
-		} else {
-			++cursor;
-		}
-	}
-
-	min_diff = std::min(
-		min_diff,
-		distance(
-			selected.at(cursor - 1), 
-			on_hold.at(on_hold_cursor)
-		)
-	);
-
-	return min_diff;
+void print_vector(std::string_view name, std::vector<int64_t> v) {
+  std::cout << name << ": ";
+  for (const auto i : v) {
+    std::cout << i << " ";
+  }
+  std::cout << '\n';
 }
 
-void
-print_vector(std::string_view name, std::vector<int64_t> v) {
-	std::cout << name << ": ";
-	for (const auto i : v) {
-		std::cout << i << " ";
-	}
-	std::cout << '\n';
-}
+int main() {
+  std::vector<int64_t> A{12, 25, 17, 5, 1};
+  std::vector<int64_t> B{-5, 30, 10};
 
-int 
-main() {
-    std::vector<int64_t> A{12, 25, 17, 5, 1};
-	std::vector<int64_t> B{-5, 30, 10};
+  print_vector("A", A);
+  print_vector("B", B);
 
-	print_vector("A", A);
-	print_vector("B", B);
+  insertion_sort(A);
+  insertion_sort(B);
 
-	insertion_sort(A);
-	insertion_sort(B);
+  auto const min_diff = shortest_distance(A, B);
 
-	auto const min_diff = shortest_distance(A, B);
+  std::cout << "SHORTEST DISTANCE: " << min_diff << '\n';
 
-	std::cout << "SHORTEST DISTANCE: " << min_diff << '\n';
-    
-    return 0;
+  return 0;
 }
